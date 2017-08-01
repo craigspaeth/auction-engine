b03591389fa7bd4af3fd830ab61368a20b823968
Artsy <craigspaeth@gmail.com>
2017-08-01 00:44:02 -0400

    Got bidding wired up

--- README.md
+++ README.md
@@ -1,2 +1,39 @@
 # auction-engine
+
 Spike project for an auction engine
+
+## Getting Started
+
+Install and start mongo
+
+```
+$ brew install mongo
+$ mongod
+```
+
+Copy envrionment, install modules, start server
+
+```
+$ cp .env.example .env
+$ npm i
+$ npm start
+```
+
+Point Prediction's Causality urls to this in the .env file
+
+```
+CAUSALITY_BIDDERS_BASE_URL=http://localhost:3000
+CAUSALITY_BIDDERS_WEBSOCKET_URL=ws://localhost:3000
+CAUSALITY_OBSERVERS_BASE_URL=http://localhost:3000
+CAUSALITY_OBSERVERS_WEBSOCKET_URL=ws://localhost:3000
+```
+
+Start Prediction
+
+```
+$ yarn start
+```
+
+## Liscence
+
+MIT
--- index.js
+++ index.js
@@ -6,19 +6,24 @@ const httpProxy = require('http-proxy')
 const proxy = httpProxy.createProxyServer()
 const jwt = require('jsonwebtoken')
 const uid = require('uid')
+const _ = require('lodash')
 
-const { DB_URL, PORT, HMAC_SECRET } = process.env
+const {
+  DB_URL,
+  PORT,
+  HMAC_SECRET,
+  CAUSALITY_URL,
+  INCREMENT_POLICY: IP
+} = process.env
 const { ObjectId } = pmongo
+const INCREMENT_POLICY = JSON.parse(IP)
 const db = pmongo(DB_URL, ['lots'])
 const app = express()
 const server = http.createServer(app)
 const wss = new WebSocket.Server({ server })
 
 app.use((req, res) => {
-  proxy.web(req, res, {
-    target: 'https://causality-bidders-staging.artsy.net',
-    changeOrigin: true
-  })
+  proxy.web(req, res, { target: CAUSALITY_URL, changeOrigin: true })
 })
 
 wss.on('connection', (ws, req) => {
@@ -63,9 +68,9 @@ wss.on('connection', (ws, req) => {
       // Handles new events added to the lot event list. This is where the
       // auction calculator and derived state is run.
       //
-      // Send an initial accepted message event
+      // Send an initial "accepted" message
       send({ type: 'CommandSuccessful', wasAccepted: true })
-      const _id = ObjectId(msg.lotId)
+      const _id = ObjectId(msg.event.lotId)
       // Create the lot if it doesn't exist
       // TODO: Sync initial lot data on bootup instead
       await db.lots.update(
@@ -73,82 +78,91 @@ wss.on('connection', (ws, req) => {
         { $setOnInsert: { events: [] } },
         { upsert: true }
       )
-      // Transform the event into a DB appropriate model
+      // Transform the event into a database appropriate model
       const event = {
         FirstPriceBidPlaced: () => ({
           id: uid(),
           type: 'bid',
-          ammount: msg.event.amountCents,
-          user: _.pick(user, 'id', 'role', '')
+          amount: msg.event.amountCents,
+          user: _.pick(user, 'id', 'role', 'bidderId')
         })
       }[msg.event.type]()
-      // Validate the event can be appended and save to database
+      // Validate the event can be appended and saved to database
+      // TODO: Validation code
       const lot = await db.lots.findOne({ _id })
-      console.log(
-        `${JSON.stringify(event)} valid to add to ${JSON.stringify(lot.events)}?`
-      )
       const lotEvents = lot.events.concat(event)
       await db.lots.update({ _id }, { $set: { events: lotEvents } })
       // Reduce lot event list into the Causality LotUpdateBroadcast data model.
       // In the future one could imagine going further and reducing this list
       // of events into an even more useful derived state such as `youreWinning`
       // using the `user` state above to compare with the winning bid in the
-      // event list (itself a function of the event list). This logic should
-      // obviously be extracted into a "model" and mostly pure functions.
+      // event list (itself a function of the event list).
+      const winningBid = lotEvents
+        .filter(event => event.type === 'bid')
+        .reduce((winningBid, bid) => {
+          return bid.amount >= winningBid.amount ? bid : winningBid
+        })
+      const currentIncrement = INCREMENT_POLICY.filter(
+        increment =>
+          winningBid.amount >= increment.from &&
+          winningBid.amount <= increment.to
+      )[0]
+      const askingPrice = winningBid.amount + currentIncrement.amount
+      const totalBids = lotEvents
+        .filter(event => event.type === 'bid')
+        .reduce(count => count + 1, 0)
       const derivedLotState = {
-        askingPriceCents: lotEvents
-          .filter(event => event.type === 'bid')
-          .reduce((highestBid, val) => {
-            return val.ammount >= highestBid ? val.ammount : highestBid
-          }, 0),
-        bidCount: lotEvents
-          .filter(event => event.type === 'bid')
-          .reduce((count, val) => {
-            return count + 1
-          }, 0),
+        askingPriceCents: askingPrice,
+        sellingPriceCents: winningBid.amount,
+        bidCount: totalBids,
+        floorAskingPriceCents: askingPrice,
+        floorSellingPriceCents: winningBid.amount,
+        floorWinningBidder: winningBid.user,
+        winningBidEventId: winningBid.id,
+        // TODO: Stubbed data below—should also reduce state like above
         biddingStatus: 'OnBlock',
-        floorAskingPriceCents: 2000,
         floorIsOpen: true,
-        floorSellingPriceCents: 3000,
-        floorWinningBidder: { type: 'OfflineBidder' },
-        sellingPriceCents: 3000,
-        soldStatus: 'ForSale',
-        winningBidEventId: 'abc-123-456'
+        soldStatus: 'ForSale'
       }
-      const events = lotEvents.map(event => {
-        if (event.type === 'bid') {
-          return {
-            eventId: event.id,
-            amountCents: event.ammount,
-            bidder: {
-              bidderId: event.userId,
-              paddleNumber: ,
-              type: 'ArtsyBidder'
-            },
-            eventId: 'abc-123',
-            lotId: 'abc',
-            type: 'FirstPriceBidPlaced'
+      const events = lotEvents
+        .map(event => {
+          if (event.type === 'bid') {
+            return {
+              eventId: event.id,
+              amountCents: event.amount,
+              bidder: {
+                bidderId: event.user.bidderId,
+                paddleNumber: event.user.bidderId,
+                type: {
+                  bidder: 'ArtsyBidder',
+                  operator: 'ArtsyOperator',
+                  observer: 'ArtsyObserver'
+                }[event.user.role]
+              },
+              lotId: _id,
+              type: 'FirstPriceBidPlaced'
+            }
+          } else {
+            throw new Error(`Unknown lot event ${JSON.stringify(event)}`)
           }
-        }
-      })
-      const fullEventOrder = ['abc-123']
+        })
+        .reduce((map, event) => {
+          return _.assign(map, { [event.eventId]: event })
+        }, {})
       const lotUpdateBroadcast = {
         type: 'LotUpdateBroadcast',
         lotId: _id,
+        fullEventOrder: _.map(lotEvents, 'id'),
         derivedLotState,
-        events,
-        fullEventOrder
+        events
       }
       // Emit LotUpdateBroadcast (could add things like RabbitMQ here too)
-      console.log('sending...', lotUpdateBroadcast)
       send(lotUpdateBroadcast)
     } else {
-      // Unknown
-      console.log(`Unknown message ${message}`)
+      // Unknown websocket message
+      console.error(`Unknown message ${message}`)
     }
   })
 })
 
-server.listen(3000, () => {
-  console.log(`Listening on ${PORT}`)
-})
+server.listen(3000, () => console.log(`Listening on ${PORT}`))
